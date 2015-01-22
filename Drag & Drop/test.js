046e2085c39585906e79b42488509ca6fef6b51e
Tiyop <ayoubsamaki@gmail.com>
2015-01-22 17:25:00 +0100

    Drag and Drop V2
    - Version plus sexy
    - Correction de bug

--- Drag & Drop/test.js
+++ Drag & Drop/test.js
@@ -1,47 +1,45 @@
 $(function() {
 
+	var all=true;
+	var test=true;
 
+// Définition des éléments draggables/droppable ainsi que l'événement d'écriture qui survient lors d'un drop
 	$(".draggable").draggable();
 	$(".droppable").droppable({
     /*drop: function(event, ui) {
       $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
     },*/
-    over: function(event, ui) {
+    drop: function(event, ui) {
       $('.display').html( this.id );
       $('.verif', this).text(ui.draggable.select('p', this).text());
     }
 	});
 
-	var all=true;
-	var test=true;
-
-	/*$('#verify').click(function(){
-   		all = ($('#SAV .verif').text().trim() == $('#SAV').attr('id'));
-
-		if(all){
-			$('#correction p:first').text("test reussi");
-		}else {
-			$('#correction p:first').text("T'es con !");
-		}
-	});*/
-
-
+// Clignotement lors du drag
+	/*$( ".draggable" ).mouseover(function() {
+		$( this ).fadeOut( 100 );
+		$( this ).fadeIn( 500 );
+	});
+	*/
+//Vérification si toutes les ID des éléments droppés matchent avec le texte écrit à l'intérieur
 	$('#verify').click(function(){
-		$('#dropline div').each( function(index, value) { 
+		//On teste pour chaque droppable et on recalcul la variable all
+		$('#dropline div').each( function(index, value) {
 			test = ($('.verif' ,value).text().trim() == $(this).attr('id'));
     		all = all & test;
-    	if (test){
-    		$(this).css('background', 'green');
+    	//On colorie en prallèle
+			if (test){
+    		$(this).css('background', 'rgba(74, 217, 35, 0.77)');
     	}else{
-    		$(this).css('background', 'ref');
+    		$(this).css('background', 'rgba(221, 26, 26, 0.81)');
     	}
   		});
 
-
+		//On effectue un test global
 		if(all){
 			$('#correction p:first').text("Test reussi ! Bravo");
 		}else{
-			$('#correction p:first').text("C'est faux recommencez !");
+			$('#correction p:first').text("Corrigez vous pour passer à la suite...");
 		}
 	});
 
